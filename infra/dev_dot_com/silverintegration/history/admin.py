318974968fae25eee60d1aace794aa6f29e5ae41
Ryan David Hayes <ryan.david.hayes@gmail.com>
2019-01-26 00:58:34 +0000

    model change tracking

---
+++ infra/dev_dot_com/silverintegration/history/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from reversion.admin import VersionAdmin
+
+from silver.admin import *
+from silver.models import (
+    Plan, MeteredFeature, Subscription, Customer, Provider,
+    MeteredFeatureUnitsLog, Invoice, DocumentEntry,
+    ProductCode, Proforma, BillingLog, BillingDocumentBase,
+    Transaction, PaymentMethod
+)
+
+# We could add versioning to these, as well.
+
+# admin.site.unregister(PaymentMethod, PaymentMethodAdmin)
+# admin.site.unregister(Plan, PlanAdmin)
+# admin.site.unregister(Subscription, SubscriptionAdmin)
+# admin.site.unregister(Provider, ProviderAdmin)
+# admin.site.unregister(Proforma, ProformaAdmin)
+# admin.site.unregister(ProductCode)
+# admin.site.unregister(MeteredFeature)
+
+try:
+    admin.site.unregister(Transaction)
+    admin.site.unregister(Invoice)
+    admin.site.unregister(Customer)
+except:
+    print("not registered yet")
+
+@admin.register(Transaction)
+class VersionedTransactionAdmin(VersionAdmin, TransactionAdmin):
+    pass
+
+@admin.register(Invoice)
+class VersionedInvoiceAdmin(VersionAdmin, InvoiceAdmin):
+    pass
+
+@admin.register(Customer)
+class VersionedCustomerAdmin(VersionAdmin, CustomerAdmin):
+    pass
